9ee5135e73dda790b2e71f3ecdaf24f4e860e0f3
Khairul Aqram <khairulaqram21@gmail.com>
2024-07-07 14:52:55 +0700

    fix: perbaiki provider

--- app/Providers/DatasetServiceProvider.php
+++ app/Providers/DatasetServiceProvider.php
@@ -2,22 +2,20 @@
 
 namespace App\Providers;
 
-use App\Services\AuthenticationService;
 use App\Services\DatasetService;
-use App\Services\Impl\AuthenticationServiceImpl;
-use App\Services\Impl\DatasetServiceImpl;
 use Illuminate\Support\ServiceProvider;
+use App\Services\Impl\DatasetServiceImpl;
 use Illuminate\Contracts\Support\DeferrableProvider;
 
-class AuthenticationServiceProvider extends ServiceProvider implements DeferrableProvider
+class DatasetServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     public array $singletons = [
-        AuthenticationService::class => AuthenticationServiceImpl::class,
+        DatasetService::class => DatasetServiceImpl::class
     ];
 
     public function provides(): array
     {
-        return [AuthenticationService::class];
+        return [DatasetService::class];
     }
 
     /**
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthenticationServiceProvider::class,
+    App\Providers\DatasetServiceProvider::class,
 ];

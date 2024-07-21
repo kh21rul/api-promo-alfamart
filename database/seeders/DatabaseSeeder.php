473e6c2d0b5c116ea5500f9db0764b06fcad8519
Khairul Aqram <khairulaqram21@gmail.com>
2024-07-21 21:37:32 +0700

    feat: ganti data seeder akun

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Khairul Aqram',
-            'email' => 'khairulaqram21@gmail.com',
+            'name' => 'Ima Pratiwi',
+            'email' => 'ima.200170273@mhs.unimal.ac.id',
         ]);
     }
 }
